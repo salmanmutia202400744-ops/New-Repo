69cf270271f9a3cae28b2a55e27a6c2c218fe2bc
salmanmutia202400744-ops <salmanmutia.202400744@gmail.com>
2026-04-29 00:16:39 +0800

    server

--- index.js
+++ index.js
@@ -2,23 +2,32 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
+// =======================
+// MUST BE FIRST
+// =======================
+app.use(cors({
+    origin: "*", // or "http://127.0.0.1:5500"
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "user-id"]
+}));
+
+app.use(express.json());
+
+// =======================
+// ROUTES
+// =======================
 const bookingRoutes = require("./routes/bookings");
 const dashboardRoutes = require("./routes/dashboard");
 
 app.use("/api/dashboard", dashboardRoutes);
-
-app.use(cors());
-app.use(express.json());
 app.use("/api/bookings", bookingRoutes);
 app.use("/api/users", require("./routes/users"));
 app.use("/api/flights", require("./routes/flights"));
-app.use("/api/bookings", require("./routes/bookings"));
 
-
+// =======================
 app.get("/", (req, res) => {
-    res.json({
-        message: "Elite Airways API Running"
-    });
+    res.json({ message: "Elite Airways API Running" });
 });
 
 app.listen(3000, () => {
